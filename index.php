f25967767284a1501cd698648c8ec303f11a8843
Alexi RODRIGUES <alexi.r.fernandes@gmail.com>
2020-05-05 12:05:35 +0200

    modif index.php ajout paragraphe

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
-    
-    <h1 class="text-center ">Mon site GIT</h1>
-    
-    
+
+        <h1 class="text-center ">Mon site GIT</h1>
+        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit distinctio porro minima officiis necessitatibus ut nisi sed nemo voluptate numquam et provident odio natus, laboriosam aperiam, quas eos similique cupiditate?
+            Suscipit, et totam consectetur doloremque reiciendis corrupti consequuntur aliquam velit fuga dicta voluptates optio, eligendi iste, quaerat ex ducimus ipsa harum natus commodi inventore? Doloribus odio facere ullam repellat eaque.
+            Repudiandae dolores quis molestias aspernatur minus impedit adipisci temporibus voluptate esse ipsum in ab, tempora facere placeat ipsam voluptas tempore quos magni quam at officiis cupiditate? Ea itaque quae sint.
+            Voluptas molestiae cum deserunt in ratione earum nesciunt quia, quis, soluta dicta aut aperiam impedit illum quibusdam recusandae saepe, quasi laudantium. Libero sapiente cumque corrupti soluta adipisci! Temporibus, magnam libero!
+        </p>
+
+
     </div>
 </body>
+
 </html>
